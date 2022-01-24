f2f2f46f9e46227b128a0b05cb50d861bde1627f
Matheus <germano.prog@gmail.com>
2022-01-24 10:04:15 -0300

    Feat: index.js - style jsx (Next)

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function HomePage() {
+    return (
+    <div>
+        <h1>Bem vindo ao Gecord!</h1>
+        <h2>Discord - Developer Germano</h2>
+        <style jsx>{`
+        h1 {
+            color:red;
+        }
+        `}</style>
+    </div>
+    )}
+  
+  export default HomePage
