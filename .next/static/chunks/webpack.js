4e76936992151eb831e8a373732dea8a26751de2
Matheus <germano.prog@gmail.com>
2022-01-24 13:04:48 -0300

    Feat: Home configurada e estilizada

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3290d13e8644fa7"; }
+/******/ 		__webpack_require__.h = function() { return "c635dc370090d0b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
