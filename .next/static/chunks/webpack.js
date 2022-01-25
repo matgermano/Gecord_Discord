3664ff8411e233ae4dd118fd3f16fac6e562823a
Matheus <germano.prog@gmail.com>
2022-01-25 12:25:53 -0300

    Feat: Pages - Chat.js + _app.js (globalStyle)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d39da63eb3361c63"; }
+/******/ 		__webpack_require__.h = function() { return "66d27c406b7c7ff3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
